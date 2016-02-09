9a47630bf604555f2885a57c6d6207570ce8201e
sean01 <sean_rogers_12@hotmail.com>
2016-02-09 19:17:05 +1300

    first commit
    Added site to git.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    // $("random-colour").hover(function(e) {
+
+    //     var randomColour = getRandomColour();
+    //     $(e.target).attr("class", randomColour);
+
+    // });
+
+    $(".random-colour").hover(function() {
+        var randomColour = getRandomColour();
+        $(this).attr("class", randomColour);
+    }, function() {
+        
+        $(this).attr("class", "red");
+    });
+});
+
+function getRandomColour(){
+
+    //Store available css classes
+    var colours = new Array("green", "purple", "yellow", "orange", "blue");
+    
+    //Give a random number from 0 to 5
+    var randomNumber = Math.floor(Math.random()*6);
+    
+    return colours[randomNumber];
+}
